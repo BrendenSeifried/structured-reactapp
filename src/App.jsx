e66108f5ad7347fe4f63ec3a904bf6e6fee3758f
BrendenSeifried <brendenseifried@gmail.com>
2022-05-17 16:01:13 -0700

    got notelist rendering made app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import Header from './components/Header';
+// import { PrivateRoute } from './components/PrivateRoute';
+
+// import Auth from './views/Auth/Auth';
+import NoteList from './views/NoteList';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Switch>
+        {/* <Route path="/auth">
+          <Auth />
+        </Route> */}
+        {/* <PrivateRoute path="/">
+          <NoteList />
+        </PrivateRoute> */}
+        <Route path="/">
+          <NoteList />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
