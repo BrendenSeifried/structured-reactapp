0654df9aff72ab09574604ea3e89b0e85ad2fe73
BrendenSeifried <brendenseifried@gmail.com>
2022-05-20 16:55:11 -0700

    wrapped in context to test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,23 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { AuthProvider } from './context/AuthContext';
+import { NoteProvider } from './context/NoteContext';
+import { UserProvider } from './context/UserContext';
 
 describe('example test', () => {
   it('first test', async () => {
-    render(<App />);
+    render(
+      <BrowserRouter>
+        <UserProvider>
+          <AuthProvider>
+            <NoteProvider>
+              <App />
+            </NoteProvider>
+          </AuthProvider>
+        </UserProvider>
+      </BrowserRouter>
+    );
     const one = screen.getByText('The big list of notes');
     expect(one).toBeInTheDocument();
   });
