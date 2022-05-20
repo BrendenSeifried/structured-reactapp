05c496f5ab8b79eadce933eef22060e24ae19c63
BrendenSeifried <brendenseifried@gmail.com>
2022-05-20 16:37:55 -0700

    removing test for CI

--- src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-describe('example test', () => {
-  it('first test', async () => {
-    render(<App />);
-    screen.debug();
-  });
-});
