f17496fc862ac7d094095fb1a0bbd47964216fe5
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-05-20 18:09:20 -0700

    removed screen.debug from test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 describe('example test', () => {
   it('first test', async () => {
     render(<App />);
-    screen.debug();
+    
   });
 });
