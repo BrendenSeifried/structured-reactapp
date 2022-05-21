b525f38c5c45dacc12ff1c767a31d8f12d597c04
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-05-20 19:57:45 -0700

    changed supabase settings on node,client,env

--- src/services/client.js
+++ src/services/client.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 export const client = createClient(
-  process.env.REACT_APP_SUPABASE_API_URL,
-  process.env.REACT_APP_SUPABASE_API_KEY
+  process.env.SUPABASE_API_URL,
+  process.env.SUPABASE_API_KEY
 );
 
 export const checkError = ({ data, error }) => {
@@ -9,3 +9,4 @@ export const checkError = ({ data, error }) => {
 
   return data;
 };
+
